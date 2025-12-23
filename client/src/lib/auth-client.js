8756e00a16e1272f8a9c475c62eec663d0653728
RviP9973 <rp031776@gmail.com>
2025-12-24 00:21:18 +0530

    final

--- client/src/lib/auth-client.js
+++ client/src/lib/auth-client.js
@@ -1,7 +1,6 @@
 import { createAuthClient } from "better-auth/react"
-import { HOST } from "@/utils/constants"
 
 export const authClient = createAuthClient({
-    baseURL: HOST,
+    baseURL: window.location.origin,
     credentials: "include",
 })
